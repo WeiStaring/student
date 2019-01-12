cd2d3408966c87db7a04c790139b5db554a19055
伟星 林 <571843783@qq.com>
2019-01-12 09:22:29 +0800

    basic class

---
+++ Project1/Project1/标头.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<string>
+#include<string.h>
+using namespace std;
+class Graduate {
+private:
+	string uid;
+	string name;
+	bool sex;
+	int age;
+	int score;
+};
+class Gradute_a
+{
+public:
+
+private:
+
+};
+class Gradute_e
+{
+public:
+	Gradute_e();
+	~Gradute_e();
+
+private:
+
+};
+
+Gradute_e::Gradute_e()
+{
+}
+
+Gradute_e::~Gradute_e()
+{
+}
